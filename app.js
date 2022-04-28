40c36fe825c8f50286d29b7c6c068f3ccee78f0d
SJKed <simonjurstedtskolan@gmail.com>
2022-04-28 15:44:56 +0200

    made a hello world response to app.get

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const express = require('express')
+const express = require('express');
+require('dotenv').config();
+
 const PORT = process.env.PORT || 3000
 
-
 const app = express()
+app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    })
+})
 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
