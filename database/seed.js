c0d4e339d2b4456a7588fea6df957b387d8b9fca
oskarskold <Oskar.skold4@hotmail.com>
2022-04-28 17:31:02 +0200

    Test

--- database/seed.js
+++ database/seed.js
@@ -13,10 +13,18 @@ User.bulkCreate([
         userEmail: 'Arne@arenab.arne',
         userRole: 'client',
         taskId: 1
+    },
+    {
+        userName: 'XdXDSON',
+        userPassword: 'XdXDSON',
+        userEmail: 'XdXDSON@XdXDSON.XdXDSON',
+        userRole: 'client',
+        taskId: 1
     }
 ]);
 
-Task.create({
+Task.bulkCreate([
+    {
     taskName: 'Fix my porch',
     taskDescription: 'I need to fix my porch please help im helpless please i need help please',
     taskStatus: 'Incomplete',
@@ -24,13 +32,31 @@ Task.create({
     taskCreatedAt: new Date(),
     taskUpdatedAt: new Date(),
     taskCompletedAt: new Date(),
-});
+},
+{
+    taskName: 'Fix my car',
+    taskDescription: 'I need to fix my car please help im helpless please i need help please',
+    taskStatus: 'Incomplete',
+    taskImage: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=62',
+    taskCreatedAt: new Date(),
+    taskUpdatedAt: new Date(),
+    taskCompletedAt: new Date(),
+},
 
-TaskMessage.create({
+]);
+
+TaskMessage.bulkCreate([
+    {
     messageContent: 'I need help please',
     userId: 2,
     taskId: 1
-});
+},
+{
+    messageContent: 'I will help you xD',
+    userId: 3,
+    taskId: 1
+},
+]);
 
 
 
