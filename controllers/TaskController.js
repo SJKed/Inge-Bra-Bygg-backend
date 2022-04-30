3321195bf7fe044ce28e69d78b0098622022ee05
SJKed <simonjurstedtskolan@gmail.com>
2022-04-30 22:01:47 +0200

    add getMessages with authorization

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -1,8 +1,11 @@
 const Task = require('../models/task')
+const taskMessage = require('../models/taskMessage')
+const User = require('../models/user')
 
 module.exports = {
+
+    //Tasks
     getTasks: async (req, res) => {
-        console.log(req.user)
         if (req.user.userRole == 'admin') {
             tasks = await Task.findAll({})
         }
@@ -14,7 +17,6 @@ module.exports = {
         }
         res.json(tasks)
     },
-
     createTask: async (req, res) => {
         if (req.user.userRole == 'client') { throw new Error('You are not authorized to create a task') }
         if (req.user.userRole == 'worker' || req.user.userRole == 'admin') {
@@ -22,15 +24,26 @@ module.exports = {
             res.json('Task created successfully: ' + task.taskName)
         }
     },
-
     updateTask: async (req, res) => {
-        console.log(req.user + req.body + req.params)
+        const { id } = req.params
+        const task = await Task.findByPk(id)
+
+        if (req.user.userRole == 'client') { throw new Error('You are not authorized to update a task') }
+
+        await task.update(req.body, { where: { id } })
+        res.json('Task updated successfully: ' + task.taskName + ' updated by ' + req.user.userName)
+    },
+
+
+    //Task/:id/message
+    getMessages: async (req, res) => {
         const { id } = req.params
         const task = await Task.findByPk(id)
         
-        if (req.user.userRole == 'client') { throw new Error('You are not authorized to update a task') }
-        
-        await task.update(req.body, { where: { id } })
-        res.json('Task updated successfully: ' + task.taskName + ' updated by ' + req.user.userName)
+        if(req.user.userRole == 'client' && task.clientId != req.user.userId) { throw new Error('You are not authorized to view this task') }
+        if(req.user.userRole == 'worker' && task.workerId != req.user.userId) { throw new Error('You are not authorized to view this task') }
+
+        const messages = await taskMessage.findAll({ where: { taskId: id } })
+        res.json(messages)
     }
 }
--- routes/tasks.js
+++ routes/tasks.js
@@ -6,21 +6,35 @@ const Validations = require('../validations');
 
 const router = new Router();
 
+
+//Task info
 router.get('/',
     Auth.user,
     asyncHandler(TaskController.getTasks)
 );
-
 router.post('/',
     Auth.user,
     Validations.createTask,
     asyncHandler(TaskController.createTask)
 );
-
 router.patch('/:id',
     Auth.user,
     Validations.updateTask,
     asyncHandler(TaskController.updateTask)
+);
+
+//Task/:id/message
+router.get('/:id/messages',
+    Auth.user,
+    asyncHandler(TaskController.getMessages)
+);
+router.post('/:id/message',
+    Auth.user,
+    asyncHandler(TaskController.createMessage)
+);
+router.patch('/:id/message',
+    Auth.user,
+    asyncHandler(TaskController.updateMessage)
 );
 
 module.exports = router
