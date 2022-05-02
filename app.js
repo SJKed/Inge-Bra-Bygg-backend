2844436bd0cbeb85d58a663386315a95b73337e2
oskarskold <Oskar.skold4@hotmail.com>
2022-05-02 12:47:34 +0200

    Add some error handlers and started on image upload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const routes = require('./routes');
+const {errorHandler} = require('./errors/errorHandler')
 
 const PORT = process.env.PORT || 3000
 
@@ -13,6 +14,8 @@ app.use( express.urlencoded({ extended: true }) );
 app.use('/api', routes.auth);
 app.use('/api/tasks', routes.tasks);
 app.use('/api/users', routes.users);
+app.use('/api/images', routes.images)
+app.use( errorHandler )
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
