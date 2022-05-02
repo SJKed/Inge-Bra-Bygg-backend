e6637cb3f92383379a6d90f6105205574933a60d
Simon Jurstedt <89448492+SJKed@users.noreply.github.com>
2022-05-02 13:24:38 +0200

    Update images.js

--- routes/images.js
+++ routes/images.js
@@ -4,12 +4,15 @@ const ImageController = require('../controllers/ImageController')
 const router = new Router()
 const fileUpload = require('express-fileupload')
 
-router.get('/', Auth.admin, ImageController.getAll)
+router.get('/', 
+  Auth.admin, 
+  ImageController.getAll
+);
+
 router.post('/',
-  
   Auth.admin, 
   fileUpload({useTempFiles:true}),
   ImageController.upload
-)
+);
 
 module.exports = router
